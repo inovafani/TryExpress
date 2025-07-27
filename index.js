e5abccea1c35b96cae309ceafe0a85986f58efa5
inovafani <inoveka@gmail.com>
2025-07-28 02:53:32 +0700

    Still trying

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/users.js';
 import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 const port = 3000;
-const mongoDBURL = 'mongodb://127.0.01:27017/tryexpressDB';
+const mongoDBURL = 'mongodb://localhost:27017/tryexpress';
 
 app.use(bodyParser.json());
 
@@ -13,13 +14,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.use('/users', userRoutes);
+
+async function ConnectDB() {
+  const client = new MongoClient(mongoDBURL);
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('MongoDB connection erroir', error);
+  }
+}
+
+ConnectDB();
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-app.use('/users', userRoutes);
-
-mongoose
-  .connect(mongoDBURL)
-  .then(() => console.log('MongoDB Connected'))
-  .catch((err) => console.error('Mongo connection error:', err));
